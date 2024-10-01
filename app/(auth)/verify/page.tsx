d088383b6f83c946601660d2b2d95ae0c0d806c5
Reece Ward <46360824+reecenward@users.noreply.github.com>
2024-10-01 13:54:23 -0700

    Deleted verify page

--- app/(auth)/verify/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function page() {
-  return (
-   <main className="text-center">
-        <h2>Thanks for regestring!</h2>
-        <p>Before logging in, please verify your email address.</p>
-   </main>
-  )
-}
